cf37b2f71ec56e8784814a9ce43bce539a16f862
san <sandeep_hsn@yahoo.com>
2018-02-14 16:52:41 +0530

    Don't start snapshot if there's already one pending

--- snap/snapy1.py
+++ snap/snapy1.py
@@ -14,6 +14,12 @@ def filter_instances(project):
     else:
         instances=ec2.instances.all()
     return instances
+
+def has_pending_snapshot(volume):
+    snapshots=list(volume.snapshots.all())
+    return snapshots and snapshots[0].state== 'pending'
+
+
 @click.group()
 def cli():
     "sandy manages snapshots"
@@ -84,6 +90,9 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("  skipping {0}, snapshot already in progress".format(v.id))
+                continue
             print("  creating snapshots of {0}".format(v.id))
             v.create_snapshot(Description="Created by snapshot analyzer 001")
 
